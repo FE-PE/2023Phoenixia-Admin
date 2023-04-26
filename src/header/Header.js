07aba99ed77066a94e6abbbe6264b3ba147d9c77
wwan13 <wwan13@naver.com>
2023-04-27 00:28:13 +0900

    [feat]: header

---
+++ src/header/Header.js
@@ -0,0 +1,16 @@
+import './header.css'
+
+function Header() {
+
+    return (
+
+        <div className='header'>
+            <p className='logo'>2023 Phoenixia</p>
+            <img className='likelion-logo' src='img/likelionsch_logo_long.png'></img>
+        </div>
+
+    );
+
+}
+
+export default Header;
