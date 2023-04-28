eb705b09a5e9036f0cbf774cf344c4bac23c9aa1
wwan13 <wwan13@naver.com>
2023-04-28 16:41:50 +0900

    [rename]: change directory structure

--- src/main/contents/Contents.js
+++ src/main/contents/Contents.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './contents.css'
-import Trucklist from './foodtrucks/Trucklist';
+import Trucklist from './foodtrucks/trucklist/Trucklist';
 
 function Contents() {
 
